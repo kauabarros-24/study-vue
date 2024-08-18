f517b126326c6ba1bc3f85b44c0d432995175124
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-08-18 15:07:15 -0300

    Api de opções

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
-<script setup>
-import { c } from 'vite/dist/node/types.d-aGj9QkWt';
-import { ref } from 'vue';
-
-const contador = ref(0)
-
-function incrementarContador(){
-  contador.value++
-}
-
-function decrementarContador() {
-  if (contador.value > 0) {
-    contador.value--
+<script>
+  export default {
+    data() {
+      return {
+        contador: 0
+      }
+    },
+    methods: {
+      incrementarContador() {
+        this.contador++
+      },
+      decrementarContador() {
+        if (this.contador > 0) {
+          this.contador--
+        }
+      },
+      mounted() {
+        console.log(`O valor do contador é ${this.contador}`)
+      }
+    }
   }
-}
 
 </script>
 
-<template>
-  <button @click="incrementarContador">Incrementar</button> 
-  <button @click="decrementarContador">Decrementar</button>
-  <p>Valor do contador: {{ contador }}</p>
-</template>
+  <template>
+    <button @click="incrementarContador">Incrementar</button> 
+    <button @click="decrementarContador">Decrementar</button>
+    <p>Valor do contador: {{ contador }}</p>
+  </template>
 
-<style>
-button {
-  font-weight: bold;
-}</style>
+  <style scoped>
+  button {
+    font-weight: bold;
+  }
+
+  </style>
